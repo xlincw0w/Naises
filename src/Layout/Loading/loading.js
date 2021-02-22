3fd05b0b0881a108763556439cfe36bbe46ba125
xlincw0w <khaled.khazem@yahoo.com>
2021-02-22 17:55:09 +0100

    fix

--- src/Layout/Loading/loading.js
+++ src/Layout/Loading/loading.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Navbar } from 'react-bootstrap'
-import Spinner from 'react-bootstrap/spinner'
+import Spinner from 'react-bootstrap/Spinner'
 
 const Loading = () => {
     return (
-        <div className="tc" style={{ 'width': '100%', 'height': '100vh' }}>
-            <div style={{ 'height': '10vh' }}></div>
-            <Navbar.Brand><span className="orange disable-select mb3" style={{ 'fontSize': '10rem' }}>❖</span></Navbar.Brand>
-            <p className="f3 black-80 mb5">Chargement de la page ...</p>
-            <Spinner animation="border" />
+        <div className='tc' style={{ width: '100%', height: '100vh' }}>
+            <div style={{ height: '10vh' }}></div>
+            <Navbar.Brand>
+                <span className='orange disable-select mb3' style={{ fontSize: '10rem' }}>
+                    ❖
+                </span>
+            </Navbar.Brand>
+            <p className='f3 black-80 mb5'>Chargement de la page ...</p>
+            <Spinner animation='border' />
         </div>
     )
 }
